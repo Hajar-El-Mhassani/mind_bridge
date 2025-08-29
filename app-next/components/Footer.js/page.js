5a15017de2eb8a0b12ebf0372bfc692c3835e8ac
Zargarnejad <108077042+Zargarnejad@users.noreply.github.com>
2025-08-29 15:41:36 +0200

    Compeleting Socialmedia part of footer + CSS

--- app-next/components/Footer.js/page.js
+++ app-next/components/Footer.js/page.js
@@ -2,6 +2,29 @@ import React from "react";
 import styles from "./Footer.module.css";
 import Image from "next/image";
 
+const socialLinks = [
+  {
+    href: "https://facebook.com/mindbridge_ai",
+    label: "Facebook",
+    src: "/footer/socialmedia/facebook.png",
+  },
+  {
+    href: "https://twitter.com/mindbridge_ai",
+    label: "Twitter",
+    src: "/footer/socialmedia/twitter.png",
+  },
+  {
+    href: "https://linkedin.com/in/mindbridge",
+    label: "LinkedIn",
+    src: "/footer/socialmedia/linkedin.png",
+  },
+  {
+    href: "https://instagram.com/mindbridge",
+    label: "Instagram",
+    src: "/footer/socialmedia/instagram.png",
+  },
+];
+
 export default function Footer() {
   return (
     <footer className={styles.footerContainer}>
@@ -13,8 +36,8 @@ export default function Footer() {
               <Image
                 src="/footer/logo.png"
                 alt="Logo of page"
-                width={160}
-                height={40}
+                width={160}  // 10rem
+                height={40} // 2.5rem
               />
             </div>
 
@@ -29,7 +52,20 @@ export default function Footer() {
           <div className={styles.socialSection}>
             <h3 className={styles.sectionTitle}>Follow Us</h3>
             <div className={styles.socialIconsWrapper}>
-              " Social Media Icons"
+              {socialLinks.map((link) => (
+                <a
+                  key={link.href}
+                  href={link.href}
+                  className={styles.socialIconLink}
+                >
+                  <Image
+                    src={link.src}
+                    alt={link.label}
+                    width={20} //  1.25rem
+                    height={20} // 1.25rem
+                  />
+                </a>
+              ))}
             </div>
           </div>
           {/* NewsletterSection  */}
@@ -42,7 +78,6 @@ export default function Footer() {
             </p>
 
             <form className={styles.newsletterForm}>
-
               <input
                 id="email"
                 name="email"
