f2ef90309d0f66d99a03e3f4130d952f414b8935
Zargarnejad <108077042+Zargarnejad@users.noreply.github.com>
2025-09-04 20:36:18 +0200

    Fixed sidebar styles/ edit and delete buttons/ cards detail / states button style based on value

--- app-next/components/MyCourses/MyCoursesCard.jsx
+++ app-next/components/MyCourses/MyCoursesCard.jsx
@@ -1,24 +1,45 @@
 import styles from "./MyCourses.module.css";
+import { FaEdit, FaUsers, FaTrash } from "react-icons/fa";
 
-export default function CourseCard({ imageUrl, category, header, created_by, enrolled,status }) {
+export default function CourseCard({
+  imageUrl,
+  category,
+  header,
+  created_by,
+  enrolled,
+  status,
+}) {
   return (
     <div className={styles.card}>
       <div className={styles.imageWrapper}>
         <img className={styles.image} src={imageUrl} alt={header} />
       </div>
-     
+
       <div className={styles.content}>
         <div className={styles.category}>{category}</div>
         <h3 className={styles.title}>{header}</h3>
         <p className={styles.creator}>By {created_by}</p>
       </div>
       <div className={styles.detailContainer}>
-        <p className={styles.enrolled}> {enrolled} enrolled</p>
-        <div className={styles.status}> {status}</div>
+        <p className={styles.enrolled}>
+          <FaUsers /> {enrolled} enrolled
+        </p>
+        <div
+          className={
+            status === "Published" ? styles.statusPublished : styles.status
+          }
+        >
+          {" "}
+          {status}
+        </div>
       </div>
-      <div>
-        <button className={styles.button}>Edit</button>
-        <button className={styles.button}>Delete</button>
+      <div className={styles.btnContainer}>
+        <button className={styles.btnEdit}>
+          <FaEdit /> Edit
+        </button>
+        <button className={styles.btnDelete}>
+          <FaTrash /> Delete
+        </button>
       </div>
     </div>
   );
