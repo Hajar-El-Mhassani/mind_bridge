7161f8af434e191bd5ad1269aaddf4fdf68b1145
Zargarnejad <108077042+Zargarnejad@users.noreply.github.com>
2025-09-04 12:57:47 +0200

    Fixed style - footerBar

--- app-next/app/(admin)/my-courses/page.js
+++ app-next/app/(admin)/my-courses/page.js
@@ -1,10 +1,12 @@
 import MyCourses from "@/components/MyCourses/MyCourses";
 import TopBar from "@/components/TopBar/TopBar";
+import FooterBar from "@/components/FooterBar/FooterBar"
 export default function MyCoursesPage() {
   return (
     <div>
       <TopBar />
       <MyCourses />
+      <FooterBar/>
     </div>
   );
 }
