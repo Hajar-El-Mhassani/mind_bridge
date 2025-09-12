b704b345e55df981d39425e49624d906c5a34100
Hajar-El-Mhassani <hajarelmhassani14@gmail.com>
2025-09-12 08:46:29 +0200

    Do some changes

--- api/src/routers/courses.js
+++ api/src/routers/courses.js
@@ -16,7 +16,6 @@ coursesRouter.get("/courses", async (req, res) => {
       "level",
       "status",
       "category",
-      "enrolled",
       "created_by",
       "created_at",
       "updated_at"
