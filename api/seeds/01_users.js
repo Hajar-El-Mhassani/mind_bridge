d501e0b5cc97c9e2c2a193249036cce1226d597c
Hajar-El-Mhassani <hajarelmhassani14@gmail.com>
2025-09-20 04:04:15 +0200

    fix db errors

--- api/seeds/01_users.js
+++ api/seeds/01_users.js
@@ -32,84 +32,96 @@ export async function seed(knex) {
     {
       name: "Jane Doe",
       email: "jane@example.com",
-      image: "/uploads/users/default.jpg",
+      image:
+        "https://res.cloudinary.com/dg6bvmi2c/image/upload/v1758333354/Screenshot_2025-09-18_210426_gkeg8c.png",
       date_of_birth: "1990-05-15",
       password: hashedPasswords[0],
     },
     {
       name: "Arjun Mehta",
       email: "arjun@example.com",
-      image: "/uploads/users/default.jpg",
+      image:
+        "https://res.cloudinary.com/dg6bvmi2c/image/upload/v1758333669/1705780663044_ukv0ar.jpg",
       date_of_birth: "1995-05-15",
       password: hashedPasswords[1],
     },
     {
       name: "Lina Park",
       email: "lina@example.com",
-      image: "/uploads/users/default.jpg",
+      image:
+        "https://res.cloudinary.com/dg6bvmi2c/image/upload/v1758333354/Screenshot_2025-09-18_210426_gkeg8c.png",
       date_of_birth: "1992-05-15",
       password: hashedPasswords[2],
     },
     {
       name: "Samir Patel",
       email: "samir@example.com",
-      image: "/uploads/users/default.jpg",
+      image:
+        "https://res.cloudinary.com/dg6bvmi2c/image/upload/v1758320998/users/default.jpg",
       date_of_birth: "1991-05-15",
       password: hashedPasswords[3],
     },
     {
       name: "Olivia Brooks",
       email: "olivia@example.com",
-      image: "default.jpg",
+      image:
+        "https://res.cloudinary.com/dg6bvmi2c/image/upload/v1758320998/users/default.jpg",
       date_of_birth: "1990-05-15",
       password: hashedPasswords[4],
     },
     {
       name: "Rahul Verma",
       email: "rahul@example.com",
-      image: "/uploads/users/default.jpg",
+      image:
+        "https://res.cloudinary.com/dg6bvmi2c/image/upload/v1758320998/users/default.jpg",
       date_of_birth: "1989-05-15",
       password: hashedPasswords[5],
     },
     {
       name: "Amina Salah",
       email: "amina@example.com",
-      image: "/uploads/users/default.jpg",
+      image:
+        "https://res.cloudinary.com/dg6bvmi2c/image/upload/v1758320998/users/default.jpg",
       date_of_birth: "1988-05-15",
       password: hashedPasswords[6],
     },
     {
       name: "Tomasz Nowak",
       email: "tomasz@example.com",
-      image: "/uploads/users/default.jpg",
+      image:
+        "https://res.cloudinary.com/dg6bvmi2c/image/upload/v1758320998/users/default.jpg",
       date_of_birth: "1987-05-15",
       password: hashedPasswords[7],
     },
     {
       name: "Haruto Sato",
       email: "haruto@example.com",
-      image: "/uploads/users/default.jpg",
+      image:
+        "https://res.cloudinary.com/dg6bvmi2c/image/upload/v1758320998/users/default.jpg",
       date_of_birth: "1990-05-15",
       password: hashedPasswords[8],
     },
     {
       name: "Sophie Martin",
       email: "sophie@example.com",
-      image: "/uploads/users/default.jpg",
+      image:
+        "https://res.cloudinary.com/dg6bvmi2c/image/upload/v1758320998/users/default.jpg",
       date_of_birth: "1991-05-15",
       password: hashedPasswords[9],
     },
     {
       name: "Daniel Kim",
       email: "daniel@example.com",
-      image: "/uploads/users/default.jpg",
+      image:
+        "https://res.cloudinary.com/dg6bvmi2c/image/upload/v1758320998/users/default.jpg",
       date_of_birth: "1993-05-15",
       password: hashedPasswords[10],
     },
     {
       name: "Maria Rossi",
       email: "maria@example.com",
-      image: "/uploads/users/default.jpg",
+      image:
+        "https://res.cloudinary.com/dg6bvmi2c/image/upload/v1758320998/users/default.jpg",
       date_of_birth: "1992-05-15",
       password: hashedPasswords[11],
     },
